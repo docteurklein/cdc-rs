06b41ec0016e8127e0a9fe6dd40d1b05eb571a84
Florian Klein <florian.klein@free.fr>
2024-01-31 22:32:03 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,64 @@
 use std::collections::HashMap;
+use futures::future::join_all;
 use mysql::binlog::events::*;
 use mysql::{Column, Row, Conn, Opts, BinlogRequest};
 use serde_json::*;
 use serde::{Serialize};
-use postgres_types::{ToSql, Json};
+// use postgres_types::{ToSql, Json};
 use std::iter::zip;
+use google_cloud_pubsub::client::{ClientConfig, Client};
+use google_cloud_googleapis::pubsub::v1::{PubsubMessage};
+use anyhow::Result;
 
-use postgres::{Client, NoTls};
+// use postgres::{Client, NoTls};
 
-#[derive(Debug, Serialize, ToSql)]
+#[derive(Debug, Serialize)]
 struct Event {
-    table_: String,
-    name: String,
-    index_: i8,
+    db: String,
+    table: String,
     rows: Vec<EventRow>
 }
 
-#[derive(Debug, Serialize, ToSql)]
+#[derive(Debug, Serialize)]
 struct EventRow {
     before: Option<Vec<Value>>,
     after: Option<Vec<Value>>,
 }
 
-fn main() -> mysql::Result<()> {
-    let mut pg = Client::connect("host=localhost user=postgres", NoTls).unwrap();
-    let pos: u64 = 4;//pg.query_one("select max(next_position) from \"mysql binlog\".event", &[]).unwrap().get(0);
-    let mut mysql = Conn::new(Opts::from_url("mysql://root:root@127.0.0.1:49154")?)?;
+// #[derive(Debug, Serialize)]
+// struct Debezium {
+//     op: &str,
+//     schema: HashMap<_, _>,
+//     payload: {
+//         before: Option<Vec<Value>>,
+//         after: Option<Vec<Value>>,
+//     },
+// }
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // let mut pg = Client::connect("host=localhost user=postgres port=4566", NoTls).unwrap();
+    let config = ClientConfig::default().with_auth().await?;
+    let pos: u64 = 4; // pg.query_one("select max(next_position) from \"mysql binlog\".event", &[]).unwrap().get(4);
+    let mut mysql = Conn::new(Opts::from_url("mysql://root:root@127.0.0.1:3306")?)?;
     let mut binlog_stream = mysql.get_binlog_stream(
-            BinlogRequest::new(11114)
-            .with_filename("de9f0c68a241-bin.000001".as_bytes().to_vec())
+            BinlogRequest::new(1)
             .with_pos(pos)
-    ).unwrap();
+    )?;
     let mut tmes = HashMap::new();
     let mut events: Vec<RowsEventData> = vec!();
 
-    while let Some(event) = binlog_stream.next() {
-        let event = event.unwrap();
-        if  let Some(e) = event.read_data()? {
+    let pubsub = Client::new(config).await?;
+
+    let topic = pubsub.topic("projects/my-project-id/subscriptions/all_pims.pim_catalog_product");
+    let publisher = topic.new_publisher(None);
+
+    while let Some(Ok(event)) = binlog_stream.next() {
+        if let Some(e) = event.read_data()? {
             match e {
                 EventData::RowsEvent(ee) => {
                     match ee {
                         RowsEventData::WriteRowsEvent(_) => {
-                            //dbg!(eee);
                             events.push(ee.into_owned());
                         },
                         RowsEventData::UpdateRowsEvent(_) => {
@@ -57,37 +74,31 @@ fn main() -> mysql::Result<()> {
                     tmes.insert(ee.table_id(), ee.into_owned());
                 }
                 EventData::XidEvent(ee) => {
-                    let rows = events.iter().enumerate().map(|(index, event)| Event {
-                        table_: tmes[&event.table_id()].table_name().to_string(),
-                        name: "writerows".to_string(), // todo
-                        index_: index as i8,
-                        rows: event.rows(&tmes[&event.table_id()]).map(|r| {
-                            match r {
-                                Ok((before, after)) => {
-                                    EventRow {
-                                        before: before.map(|before| Some(Row::try_from(before).unwrap().unwrap().iter().map(|a| json!(a.as_sql(true))).collect())).unwrap_or(None),
-                                        after: after.map(|after| Some(Row::try_from(after).unwrap().unwrap().iter().map(|a| json!(a.as_sql(true))).collect())).unwrap_or(None)
+                    for rowsEvent in events.iter() {
+                        dbg!(&ee.xid);
+                        dbg!(&event.header().log_pos());
+
+                        let msgs: Vec<PubsubMessage> = rowsEvent.rows(&tmes[&rowsEvent.table_id()]).map(|row| {
+                             match row {
+                                Ok((before, Some(after))) => {
+                                    for col in after.columns_ref() {
+                                        dbg!(col.name_str());
+                                    }
+                                    // dbg!(&after.columns_ref().iter().map(|c| c.name_ref().into()).collect::<Vec<String>>());
+
+                                    PubsubMessage {
+                                       data: "test".into(),
+                                       // ordering_key: "order".into(),
+                                       ..Default::default()
                                     }
                                 },
-                                _ => panic!("{:?}", r)
+                                _ => panic!("{:?}", row)
                             }
-                        }).collect()
-                    }).collect();
-                    dbg!(&ee.xid);
-                    dbg!(&event.header().log_pos());
-                    pg.execute("insert into \"mysql binlog\".event
-                        (tenant, table_, name, xid, timestamp_, next_position, index_, rows)
-                        select $1, table_, name, $2, $3, $4, index_, rows
-                        from jsonb_populate_recordset(null::\"mysql binlog\".event, $5) _
-                        on conflict do nothing;
-                    ",
-                    &[
-                        &"tenant#1",
-                        &(ee.xid as i64),
-                        &(event.header().timestamp() as i64),
-                        &(event.header().log_pos() as i64),
-                        &Json::<Vec<Event>>(rows),
-                    ]).unwrap();
+                        }).collect();
+
+                        let mut awaiters = publisher.publish_bulk(msgs).await;
+                        // join_all(awaiters.iter().map(|awaiter| awaiter.get()).collect::<Vec<_>>()).await;
+                    }
                     events.clear();
                 }
                 _ => {}

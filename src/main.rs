479d6773c770af641925134f44a565f5dc21653f
Florian Klein <florian.klein@free.fr>
2024-02-06 18:05:58 +0100

    yay

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::str::from_utf8;
 use std::borrow::Cow;
 use mysql::binlog::events::*;
@@ -7,6 +7,7 @@ use mysql::binlog::EventType;
 use mysql::consts::ColumnType;
 use mysql::{Column, Row, Conn, Opts, BinlogRequest, from_value};
 use serde_json::*;
+use serde::{Serialize};
 use google_cloud_pubsub::client::{ClientConfig, Client};
 use google_cloud_googleapis::pubsub::v1::{PubsubMessage};
 use google_cloud_pubsub::publisher::Publisher;
@@ -15,7 +16,7 @@ use anyhow::anyhow;
 use regex::Regex;
 use sqlite;
 use clap::{Parser};
-use rhai::{CallFnOptions, Dynamic, Engine, Scope, AST};
+use rhai::{CallFnOptions, Dynamic, Engine, Scope, AST, CustomType, TypeBuilder};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -35,19 +36,25 @@ struct Args {
     script: Option<PathBuf>,
 }
 
+// #[derive(Debug, Clone, CustomType, Serialize)]
+// #[rhai_type()]
+// struct Event {
+//     after: BTreeMap<String, Value>,
+// }
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Args::parse();
 
-    let rhai = Engine::new();
+    let mut rhai = Engine::new();
+    // rhai.build_type::<Event>();
+
     let mut scope = Scope::new();
     let ast = cli.script.map(|path| {
         rhai.compile_file_with_scope(&scope, path)
     }).unwrap().unwrap();
 
-    let options = CallFnOptions::new().eval_ast(false).rewind_scope(false);
-    let result = rhai.call_fn_with_options::<(String)>(options, &mut scope, &ast, "message", ("message",));
-    dbg!(&result);
+    // let options = CallFnOptions::new().eval_ast(false).rewind_scope(false);
 
     let connection = sqlite::open(cli.state)?;
     connection.execute("create table if not exists log_pos (server_id integer primary key, pos integer not null, filename text not null) strict")?;
@@ -61,6 +68,8 @@ async fn main() -> Result<()> {
         }
     }).unwrap_or((4, "".into()));
     dbg!(&pos, &filename);
+
+    let mut new_filename: String = filename.clone();
     
     let mut mysql = Conn::new(Opts::from_url(&cli.source)?)?;
     let mut binlog_stream = mysql.get_binlog_stream(
@@ -74,22 +83,7 @@ async fn main() -> Result<()> {
         ClientConfig::default().with_auth().await?
     ).await?;
 
-    let mut publishers: HashMap<String, Publisher> = HashMap::new();
-
-    let colMap = HashMap::from([
-        ("pim_catalog_product", HashMap::from([
-            ("@0", "uuid"),
-            ("@1", "family_id"),
-            ("@2", "product_model_id"),
-            ("@3", "family_variant_id"),
-            ("@4", "id"),
-            ("@5", "is_enabled"),
-            ("@6", "identifier"),
-            ("@7", "raw_values"),
-            ("@8", "created"),
-            ("@9", "updated"),
-        ])),
-    ]);
+    let mut publishers: BTreeMap<String, Publisher> = BTreeMap::new();
 
     while let Some(event) = binlog_stream.next() {
         let event = event.unwrap();
@@ -98,17 +92,23 @@ async fn main() -> Result<()> {
             let msgs: Vec<PubsubMessage> = vec!();
             match e {
                 EventData::RotateEvent(e) => {
-                    filename = e.name().to_string();
+                    new_filename = e.name().to_string();
                 }
+                // EventData::TableMapEvent(e) => {
+                //     tmes.insert(e.table_id(), e);
+                // }
                 EventData::RowsEvent(rowsEvent) => {
-                    let tme = &binlog_stream.get_tme(rowsEvent.table_id());
+                    // let tme = tmes.get(&rowsEvent.table_id()).unwrap();
+                    let tme = binlog_stream.get_tme(rowsEvent.table_id());
+                    // let tme = tmes.get(rowsEvent.table_id()).unwrap();
                     if tme.is_none() {
                         continue;
                     }
                     let tme = tme.unwrap();
+                    let (db, table) = (tme.database_name().to_string(), tme.table_name().to_string());
 
                     let re = Regex::new(&cli.regex).unwrap();
-                    if ! re.is_match(&format!("{}.{}", tme.database_name(), tme.table_name())) {
+                    if ! re.is_match(&format!("{}.{}", db, table)) {
                         continue;
                     }
 
@@ -118,63 +118,71 @@ async fn main() -> Result<()> {
                         RowsEventData::DeleteRowsEvent(_) => "DELETE",
                         _ => ""
                     };
+
                     match rowsEvent {
                         RowsEventData::WriteRowsEvent(_) |
                         RowsEventData::UpdateRowsEvent(_) |
                         RowsEventData::DeleteRowsEvent(_) => {
-                            let msgs: Vec<PubsubMessage> = rowsEvent.rows(tme).map(|row| {
+                            let msgs: Vec<PubsubMessage> = rowsEvent.clone().rows(tme).map(|row| {
                                  match row {
                                     Ok((before, after)) => {
                                         let rowImage = after.or(before);
-                                        let cols: Vec<(String, ColumnType)> = rowImage.clone().map(|rowImage| {
+
+                                        let cols: Vec<String> = rowImage.as_ref().map(|rowImage| {
                                             rowImage.columns_ref().iter().map(|c| {
-                                                let col = c.name_str();
-                                                dbg!(&col);
-                                                let colName = colMap.get(tme.table_name().as_ref())
-                                                    .map_or(col.as_ref(), |t| t.get(&col.as_ref()).unwrap_or(&col.as_ref()))
-                                                ;
-                                                (colName.to_string(), c.column_type())
+                                                c.name_str().to_string()
                                             }).collect()
                                         }).unwrap();
                                         
-                                        let mut data: Map<String, Value> = Row::try_from(rowImage.unwrap())
+                                        let mut fields: rhai::Map = Row::try_from(rowImage.unwrap())
                                             .unwrap() // Result
                                             .unwrap() // as Vec
                                             .iter()
                                             .enumerate()
                                             .map(|(i, mv)| {
-                                                let (k, typ) = cols.get(i).unwrap();
+                                                let k = cols.get(i).unwrap();
                                                 let v = match mv {
-                                                    mysql::Value::NULL => json!(null),
+                                                    mysql::Value::NULL => Dynamic::from(()),
                                                     mysql::Value::Bytes(ref bytes) => match from_utf8(&*bytes) {
-                                                        Ok(string) => json!(string),
+                                                        Ok(v) => Dynamic::from(v.to_string()),
                                                         Err(_) => {
                                                             let mut s = String::from("0x");
                                                             for c in bytes.iter() {
                                                                 s.extend(format!("{:02X}", *c).chars())
                                                             }
-                                                            json!(s)
+                                                            Dynamic::from(s)
                                                         },
                                                     }
-                                                    mysql::Value::Int(v) => json!(v),
-                                                    mysql::Value::UInt(v) => json!(v),
-                                                    mysql::Value::Float(v) => json!(v),
-                                                    mysql::Value::Double(v) => json!(v),
-                                                    // mysql::Value::Date(v) => json!(v),
-                                                    // mysql::Value::Time(v) => json!(v),
-                                                    _ => json!("NOPE"),
+                                                    mysql::Value::Int(v) => Dynamic::from(v.clone()),
+                                                    mysql::Value::UInt(v) => Dynamic::from(v.clone()),
+                                                    mysql::Value::Float(v) => Dynamic::from(v.clone()),
+                                                    mysql::Value::Double(v) => Dynamic::from(v.clone()),
+                                                    // mysql::Value::Date(v) => Dynamic::from(v),
+                                                    // mysql::Value::Time(v) => Dynamic::from(v),
+                                                    _ => Dynamic::from("NOPE"),
                                                 };
-                                                (k.to_string(), v)
+                                                (k.into(), v)
                                             })
                                             .collect()
                                         ;
-                                        data.insert("_CHANGE_TYPE".to_string(), changeType.into());
-                                        data.insert("db".to_string(), tme.database_name().into());
+                                        // fields.insert("_CHANGE_TYPE".into(), Dynamic::from(changeType));
+                                        // fields.insert("db".into(), Dynamic::from(db.clone()));
 
-                                        dbg!(&json!(data));
+                                        // let event = Event {
+                                        //     after: fields,
+                                        // };
+                                        // dbg!(&json!(event));
+                                        // let dynFields = fields.clone();
+
+                                        let fields = rhai
+                                            .call_fn::<rhai::Map>(&mut scope, &ast, "transform", (db.clone(), table.clone(), changeType, fields.clone()))
+                                            .map_err(|e| dbg!(e))
+                                            .unwrap_or(fields)
+                                        ;
+                                        dbg!(&fields);
 
                                         PubsubMessage {
-                                            data: json!(data).to_string().into(),
+                                            data: rhai::format_map_as_json(&fields).into(),
                                             ..Default::default()
                                         }
                                     },
@@ -183,21 +191,24 @@ async fn main() -> Result<()> {
                             }).collect();
 
                             let publisher = publishers.entry(tme.table_name().to_string()).or_insert_with(|| {
-                                let topic = pubsub.topic(&format!("projects/akecld-prd-pim-saas-fci/topics/all_pims.{}", tme.table_name().as_ref()));
-                                dbg!(&topic.fully_qualified_name());
-                                topic.new_publisher(None)
+                                let topic = rhai
+                                    .call_fn::<String>(&mut scope, &ast, "topic", (db, tme.table_name().to_string(),))
+                                    .map_err(|e| dbg!(e))
+                                    .unwrap_or(tme.table_name().to_string())
+                                ;
+                                dbg!(&topic);
+                                pubsub.topic(&topic).new_publisher(None)
                             });
                     
                             publisher.publish_immediately(msgs, None).await?;
+
+                            pos = event.header().log_pos() as i64;
+                            filename = new_filename.clone();
                         }
                         _ => {}
                     }
                 }
                 _ => {}
-            }
-
-            if event.header().log_pos() > 0 {
-                pos = event.header().log_pos() as i64;
             }
 
             let mut statement = connection.prepare("

d69c0e3d89bf9e01d1132a02aeb86e4f0cc3f399
Florian Klein <florian.klein@free.fr>
2024-02-15 09:59:46 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ async fn main() -> Result<()> {
 
     let rhai = Engine::new();
 
-    let (tx, mut rx) = mpsc::channel::<(i64, String)>(12);
+    let (log_pos_tx, mut log_pos_rx) = mpsc::channel::<(i64, String)>(1000);
 
     let mut scope = Scope::new();
     let ast = cli.script.map(|path| {
@@ -65,22 +65,49 @@ async fn main() -> Result<()> {
     let (pos, mut filename) = setup_state(&cli.state, cli.server_id)?;
     dbg!(&pos, &filename);
 
-    let connection = sqlite::open(&cli.state.clone()).unwrap();
-    let persister = tokio::spawn(async move {
-        while let Some((pos, filename)) = rx.recv().await {
+    let log_pos_persister = tokio::spawn(async move {
+        let connection = sqlite::open(&cli.state.clone()).unwrap();
+        while let Some((pos, filename)) = log_pos_rx.recv().await {
             let mut statement = connection.prepare("
                 insert into log_pos (server_id, pos, filename) values (?, max(4, ?), ?)
                 on conflict do update set
                 pos = excluded.pos,
                 filename = excluded.filename
             ").unwrap();
-            statement.bind((1, cli.server_id as i64)).unwrap();
-            statement.bind((2, pos)).unwrap();
-            statement.bind((3, filename.as_str())).unwrap();
-            statement.next().unwrap();
+            statement.bind((1, cli.server_id as i64))?;
+            statement.bind((2, pos))?;
+            statement.bind((3, filename.as_str()))?;
+            statement.next()?;
             dbg!((pos, filename));
-            ()
         }
+        Ok::<(), Error>(())
+    });
+
+    let (pubsub_tx, mut pubsub_rx) = mpsc::channel::<(&String, &String, Vec<PubsubMessage>)>(1000);
+
+    let publisher_task = tokio::spawn(async move {
+        let pubsub = Client::new(
+            ClientConfig::default().with_auth().await?
+        ).await?;
+
+        let mut publishers: BTreeMap<String, Publisher> = BTreeMap::new();
+
+        while let Some((db, table, msgs)) = pubsub_rx.recv().await {
+             let publisher = publishers.entry(table.clone().into_owned()).or_insert_with(|| {
+                 let topic = rhai
+                     .call_fn::<String>(&mut scope, &ast, "topic", (db, table.clone(),))
+                     .map_err(|e| dbg!(e))
+                     .unwrap_or(table.clone())
+                 ;
+                 dbg!(&topic);
+                 pubsub.topic(&topic).new_publisher(None)
+             });
+
+            publisher.publish_immediately(msgs, None).await?;
+        };
+
+        Ok::<(), Error>(())
+
     });
 
     let mysql = Conn::new(Opts::from_url(&cli.source)?)?;
@@ -90,11 +117,6 @@ async fn main() -> Result<()> {
             .with_filename(filename.as_bytes().to_vec())
     )?;
 
-    // let pubsub = Client::new(
-    //     ClientConfig::default().with_auth().await?
-    // ).await?;
-
-    let mut publishers: BTreeMap<String, Publisher> = BTreeMap::new();
 
     while let Some(event) = binlog_stream.next() {
         let event = event.unwrap();
@@ -103,7 +125,6 @@ async fn main() -> Result<()> {
             match e {
                 EventData::RotateEvent(e) => {
                     filename = e.name().to_string();
-                    // tx.send((event.header().log_pos() as i64, filename)).await?;
                 }
                 EventData::RowsEvent(rows_event) => {
                     let tme = binlog_stream.get_tme(rows_event.table_id()).unwrap();
@@ -151,17 +172,6 @@ async fn main() -> Result<()> {
                                 }
                             }).collect();
 
-                            // let publisher = publishers.entry(tme.table_name().to_string()).or_insert_with(|| {
-                            //     let topic = rhai
-                            //         .call_fn::<String>(&mut scope, &ast, "topic", (db, table.clone(),))
-                            //         .map_err(|e| dbg!(e))
-                            //         .unwrap_or(table.clone())
-                            //     ;
-                            //     dbg!(&topic);
-                            //     pubsub.topic(&topic).new_publisher(None)
-                            // });
-                    
-                            //publisher.publish_immediately(msgs, None).await?;
 
                         }
                         _ => {}
@@ -170,11 +180,11 @@ async fn main() -> Result<()> {
                 _ => {}
             }
             if event.header().log_pos() > 0 {
-                tx.send((event.header().log_pos() as i64, filename.clone())).await?;
+                log_pos_tx.send((event.header().log_pos() as i64, filename.clone())).await?;
             }
         }
     }
-    persister.await?;
+    log_pos_persister.await?;
 
     Ok(())
 }

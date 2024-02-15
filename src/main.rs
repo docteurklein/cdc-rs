c79ddb69bc01e234c00ea3197b8cc484ae5cf08f
Florian Klein <florian.klein@free.fr>
2024-02-15 20:49:13 +0100

    async mysql

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::convert::identity;
 use std::path::PathBuf;
 use std::collections::BTreeMap;
 use std::str::from_utf8;
-use mysql::binlog::events::*;
-use mysql::binlog::row::BinlogRow;
-use mysql::{Row, Conn, Opts, BinlogRequest};
+use mysql_async::binlog::events::*;
+use mysql_async::binlog::row::BinlogRow;
+use mysql_async::{Value, Row, Conn, Opts, BinlogStreamRequest};
 use google_cloud_pubsub::client::{ClientConfig, Client};
 use google_cloud_googleapis::pubsub::v1::PubsubMessage;
 use google_cloud_pubsub::publisher::Publisher;
@@ -13,6 +13,9 @@ use regex::Regex;
 use clap::Parser;
 use rhai::{Dynamic, Engine, Scope};
 use tokio::sync::mpsc;
+use futures::{StreamExt};
+use anyhow::anyhow;
+use std::fmt;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -30,6 +33,29 @@ struct Args {
 
     #[arg(short, long, env)]
     script: Option<PathBuf>,
+}
+
+#[derive(Clone, Debug)]
+enum ChangeType {
+    Insert,
+    Update,
+    Delete,
+    Other,
+}
+
+impl fmt::Display for ChangeType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // write!(f, "{:?}", self)
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Change {
+    op: ChangeType,
+    before: Option<BinlogRow>,
+    after: Option<BinlogRow>,
+    ts: u32,
 }
 
 fn setup_state(path: &String, server_id: u32) -> Result<(i64, String), Error> {
@@ -53,19 +79,12 @@ fn setup_state(path: &String, server_id: u32) -> Result<(i64, String), Error> {
 async fn main() -> Result<()> {
     let cli = Args::parse();
 
-    let rhai = Engine::new();
-
-    let (log_pos_tx, mut log_pos_rx) = mpsc::channel::<(i64, String)>(1000);
-
-    let mut scope = Scope::new();
-    let ast = cli.script.map(|path| {
-        rhai.compile_file_with_scope(&scope, path)
-    }).unwrap().unwrap();
+    let (log_pos_tx, mut log_pos_rx) = mpsc::unbounded_channel::<(i64, String)>();
 
     let (pos, mut filename) = setup_state(&cli.state, cli.server_id)?;
     dbg!(&pos, &filename);
 
-    let log_pos_persister = tokio::spawn(async move {
+    let log_pos_task = tokio::spawn(async move {
         let connection = sqlite::open(&cli.state.clone()).unwrap();
         while let Some((pos, filename)) = log_pos_rx.recv().await {
             let mut statement = connection.prepare("
@@ -83,7 +102,50 @@ async fn main() -> Result<()> {
         Ok::<(), Error>(())
     });
 
-    let (pubsub_tx, mut pubsub_rx) = mpsc::channel::<(&String, &String, Vec<PubsubMessage>)>(1000);
+    let (pubsub_tx, mut pubsub_rx) = mpsc::unbounded_channel::<(String, Vec<String>)>();
+
+    let (transform_tx, transform_rx) = std::sync::mpsc::channel::<(String, String, Vec<Change>)>();
+
+    let transformer_thread = std::thread::spawn(move || {
+        let rhai = Engine::new();
+        let mut scope = Scope::new();
+        let ast = cli.script.map(|path| {
+            rhai.compile_file_with_scope(&scope, path)
+        }).unwrap().unwrap();
+
+        while let Ok((db, table, changes)) = transform_rx.recv() {
+            let msgs = changes.into_iter().map(|Change {op, before, after, ts}| {
+
+                let before = before.clone().map(row_image_to_map).map_or(BTreeMap::new(), identity);
+                let after = after.clone().map(row_image_to_map).map_or(BTreeMap::new(), identity);
+
+                let fields = rhai.call_fn::<rhai::Map>(
+                    &mut scope,
+                    &ast,
+                    "transform",
+                    (
+                        db.clone(),
+                        table.clone(),
+                        op.to_string(),
+                        before,
+                        after,
+                        ts
+                    )
+                ).map_err(|e| dbg!(e))
+                .unwrap();
+
+                rhai::format_map_as_json(&fields)
+            }).collect();
+
+            let topic = rhai
+                .call_fn::<String>(&mut scope, &ast, "topic", (db.to_string(), table.to_string(),))
+                .map_err(|e| dbg!(e))
+                .unwrap_or(table.to_string())
+            ;
+
+            pubsub_tx.send((topic, msgs)).map_err(|e| dbg!(e)).unwrap();
+        }
+    });
 
     let publisher_task = tokio::spawn(async move {
         let pubsub = Client::new(
@@ -92,33 +154,32 @@ async fn main() -> Result<()> {
 
         let mut publishers: BTreeMap<String, Publisher> = BTreeMap::new();
 
-        while let Some((db, table, msgs)) = pubsub_rx.recv().await {
-             let publisher = publishers.entry(table.clone().into_owned()).or_insert_with(|| {
-                 let topic = rhai
-                     .call_fn::<String>(&mut scope, &ast, "topic", (db, table.clone(),))
-                     .map_err(|e| dbg!(e))
-                     .unwrap_or(table.clone())
-                 ;
+        while let Some((topic, msgs)) = pubsub_rx.recv().await {
+            let msgs = msgs.iter().map(|data| {
+                PubsubMessage {
+                    data: data.to_string().into(),
+                    ..Default::default()
+                }
+            }).collect();
+
+            let publisher = publishers.entry(topic.clone()).or_insert_with(|| {
                  dbg!(&topic);
                  pubsub.topic(&topic).new_publisher(None)
-             });
+            });
 
+            dbg!(&msgs);
             publisher.publish_immediately(msgs, None).await?;
         };
-
         Ok::<(), Error>(())
-
     });
 
-    let mysql = Conn::new(Opts::from_url(&cli.source)?)?;
-    let mut binlog_stream = mysql.get_binlog_stream(
-            BinlogRequest::new(cli.server_id)
-            .with_pos(pos as u32)
-            .with_filename(filename.as_bytes().to_vec())
-    )?;
+    let mysql = Conn::new(Opts::from_url(&cli.source)?).await?;
+    let mut binlog_stream = mysql.get_binlog_stream(BinlogStreamRequest::new(cli.server_id)
+        .with_pos(pos as u64)
+        .with_filename(filename.as_bytes())
+    ).await?;
 
-
-    while let Some(event) = binlog_stream.next() {
+    while let Some(event) = binlog_stream.next().await {
         let event = event.unwrap();
 
         if let Some(e) = event.read_data()? {
@@ -127,7 +188,7 @@ async fn main() -> Result<()> {
                     filename = e.name().to_string();
                 }
                 EventData::RowsEvent(rows_event) => {
-                    let tme = binlog_stream.get_tme(rows_event.table_id()).unwrap();
+                    let tme = binlog_stream.get_tme(rows_event.table_id()).ok_or(anyhow!("no tme"))?;
                     let (db, table) = (tme.database_name().to_string(), tme.table_name().to_string());
 
                     let re = Regex::new(&cli.regex).unwrap();
@@ -136,43 +197,34 @@ async fn main() -> Result<()> {
                     }
 
                     let change_type = match rows_event {
-                        RowsEventData::WriteRowsEvent(_) => "INSERT",
-                        RowsEventData::UpdateRowsEvent(_) => "UPDATE",
-                        RowsEventData::DeleteRowsEvent(_) => "DELETE",
-                        _ => ""
+                        RowsEventData::WriteRowsEvent(_) => ChangeType::Insert,
+                        RowsEventData::UpdateRowsEvent(_) => ChangeType::Update,
+                        RowsEventData::DeleteRowsEvent(_) => ChangeType::Delete,
+                        _ => ChangeType::Other,
                     };
 
                     match rows_event {
                         RowsEventData::WriteRowsEvent(_) |
                         RowsEventData::UpdateRowsEvent(_) |
                         RowsEventData::DeleteRowsEvent(_) => {
-                            let msgs: Vec<PubsubMessage> = rows_event.clone().rows(tme).map(|row| {
+                            let changes: Vec<Change> = rows_event.clone().rows(tme).map(|row| {
                                  match row {
                                     Ok((before, after)) => {
 
-                                        let before = before.clone().map(row_image_to_map).map_or(BTreeMap::new(), identity);
-                                        let after = after.clone().map(row_image_to_map).map_or(BTreeMap::new(), identity);
-
                                         let ts = event.header().timestamp();
 
-                                        let fields = rhai
-                                            .call_fn::<rhai::Map>(&mut scope, &ast, "transform", (db.clone(), table.clone(), change_type, before, after, ts))
-                                            .map_err(|e| dbg!(e))
-                                            .unwrap()
-                                        ;
-
-                                        let data = rhai::format_map_as_json(&fields);
-                                        dbg!(&data);
-                                        PubsubMessage {
-                                            data: data.into(),
-                                            ..Default::default()
+                                        Change {
+                                            op: change_type.clone(),
+                                            before,
+                                            after,
+                                            ts,
                                         }
                                     },
                                     _ => panic!("{:?}", row)
                                 }
                             }).collect();
 
-
+                            transform_tx.send((db.to_string(), table.to_string(), changes))?;
                         }
                         _ => {}
                     }
@@ -180,13 +232,14 @@ async fn main() -> Result<()> {
                 _ => {}
             }
             if event.header().log_pos() > 0 {
-                log_pos_tx.send((event.header().log_pos() as i64, filename.clone())).await?;
+                log_pos_tx.send((event.header().log_pos() as i64, filename.clone()))?;
             }
         }
     }
-    log_pos_persister.await?;
 
-    Ok(())
+    // transformer_thread.join().unwrap();
+
+    publisher_task.await?
 }
 
 fn row_image_to_map(row_image: BinlogRow) -> rhai::Map {
@@ -201,8 +254,8 @@ fn row_image_to_map(row_image: BinlogRow) -> rhai::Map {
         .map(|(i, mv)| {
             let k = cols.get(i).unwrap();
             let v = match mv {
-                mysql::Value::NULL => Dynamic::from(()),
-                mysql::Value::Bytes(ref bytes) => match from_utf8(bytes) {
+                Value::NULL => Dynamic::from(()),
+                Value::Bytes(ref bytes) => match from_utf8(bytes) {
                     Ok(v) => Dynamic::from(v.to_string()),
                     Err(_) => {
                         let mut s = String::from("0x");
@@ -212,10 +265,10 @@ fn row_image_to_map(row_image: BinlogRow) -> rhai::Map {
                         Dynamic::from(s)
                     },
                 }
-                mysql::Value::Int(v) => Dynamic::from(*v),
-                mysql::Value::UInt(v) => Dynamic::from(*v),
-                mysql::Value::Float(v) => Dynamic::from(*v),
-                mysql::Value::Double(v) => Dynamic::from(*v),
+                Value::Int(v) => Dynamic::from(*v),
+                Value::UInt(v) => Dynamic::from(*v),
+                Value::Float(v) => Dynamic::from(*v),
+                Value::Double(v) => Dynamic::from(*v),
                 // mysql::Value::Date(v) => Dynamic::from(v),
                 // mysql::Value::Time(v) => Dynamic::from(v),
                 _ => Dynamic::from("NOPE"),
